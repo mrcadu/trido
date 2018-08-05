1a1d8f926e77b15c2b1a94649160ab4990de4bf0
cadu <carlosvicc14@gmail.com>
2018-08-05 19:23:24 -0300

    criando componente para dar submit no form

---
+++ src/components/save-tarefa.js
@@ -0,0 +1,19 @@
+import React,{Component} from 'react'
+import AddTarefa from './add-tarefa'
+
+class SaveTarefa extends Component{
+    constructor(props){
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    handleSubmit = values => {
+          console.log(values)
+    };
+    render(){
+        return(
+            <AddTarefa onSubmit={this.handleSubmit} />
+        )
+    }
+}
+
+export default SaveTarefa;
