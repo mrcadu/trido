33c81069e0b79d3e89cf6340fee8d3b535b3d3cf
cadu <carlosvicc14@gmail.com>
2018-08-05 19:16:44 -0300

    modificando o text-input para poder utiliza-lo no form

--- src/components/text-input.js
+++ src/components/text-input.js
@@ -2,44 +2,28 @@ import React, {Component} from 'react';
 import PropTypes from 'proptypes';
 
 class TextInput extends Component {
-    constructor(props) {
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
-        this.state = {
-            text: ''
-        };
-    }
-
-    handleChange(e) {
-        this.setState({
-            text: e.target.value
-        });
-
-    }
-
     render() {
         return <input name={this.props.name}
                       autoFocus="true"
                       type="text"
-                      align="center"
                       placeholder={this.props.placeholder}
-                      value={this.state.text}
+                      value={this.props.value}
                       onBlur={this.props.onBlur}
-                      onChange={this.handleChange}
+                      onChange={this.props.handleChange}
                       onKeyDown={this.props.onKeyDown}
                       style={this.props.style}
                       className="text-input"/>;
     }
 }
-
 TextInput.propTypes = {
     name: PropTypes.string,
     text: PropTypes.string,
+    value: PropTypes.string,
     onBlur: PropTypes.func,
     onKeyDown: PropTypes.func,
     placeholder: PropTypes.string,
     style: PropTypes.object,
-    className: PropTypes.string
+    className: PropTypes.string,
 };
 
 export default TextInput;
