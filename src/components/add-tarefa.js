73536ad1589822b0769754dbbf90bfced2e18f07
cadu <carlosvicc14@gmail.com>
2018-08-08 09:36:30 -0300

    fazendo com que os componentes personalizados funcionem corretamente

--- src/components/add-tarefa.js
+++ src/components/add-tarefa.js
@@ -8,61 +8,101 @@ import Button from "./button";
 import {reduxForm, Field} from "redux-form";
 
 class AddTarefa extends Component {
-    constructor(props){
-        super(props)
+    constructor(props) {
+        super(props);
     }
+
     render() {
         return (
-            <form onSubmit={this.props.handleSubmit(this.props.onSubmit)}
+            <form onSubmit={this.props.handleSubmit}
                   style={{margin: '20px'}}>
+
                 <div style={{marginBottom: '20px'}}>
                     <label className="formLabel">Tarefa</label>
                     <div>
                         <Field label="tarefa"
-                               placeholder="Tarefa"
                                name="tarefa"
-                               component={TextInput}
+                               component={props =>
+                                   <TextInput
+                                       value={props.input.value}
+                                       handleChange={param => props.input.onChange(param.target.value)}
+                                       placeholder="Tarefa"/>}
                                type="text"
                                className="formElement"/>
                     </div>
                 </div>
+
                 <div style={{marginBottom: '20px'}}>
                     <label className="formLabel">Duração</label>
                     <div>
                         <Field label="duracao"
                                placeholder="Duração"
                                name="duracao"
-                               component={TextInput}
+                               component={props =>
+                                   <TextInput
+                                       value={props.input.value}
+                                       handleChange={param => props.input.onChange(param.target.value)}
+                                       placeholder="Duração"/>}
                                type="text"/>
                     </div>
                 </div>
+
                 <div style={{marginBottom: '20px'}}>
                     <label className="formLabel">Equilíbrio</label>
                     <Field label="equilibrio"
                            placeholder="Equilíbrio"
                            name="equilibrio"
-                           component={SelectMany}
+                           component={props =>
+                               <SelectMany
+                                   value={props.input.value}
+                                   options={[
+                                       {value: 'chocolate', label: 'Chocolate'},
+                                       {value: 'strawberry', label: 'Strawberry'},
+                                       {value: 'vanilla', label: 'Vanilla'}
+                                   ]}
+                                   placeholder="Duração"
+                                   handleChange = {(selectedOption) => props.input.onChange(selectedOption)}/>}
                            style={{marginBottom: '20px'}}/>
                 </div>
+
                 <div style={{marginBottom: '20px'}}>
                     <label className="formLabel"> Metas </label>
                     <Field label="metas"
                            placeholder="Metas"
                            name="metas"
-                           component={SelectOne}/>
+                           component={props =>
+                               <SelectOne
+                                   value={props.input.value}
+                                   options={[
+                                       {value: 'chocolate', label: 'Chocolate'},
+                                       {value: 'strawberry', label: 'Strawberry'},
+                                       {value: 'vanilla', label: 'Vanilla'}
+                                   ]}
+                                   placeholder="Duração"
+                                   handleChange = {(selectedOption) => props.input.onChange(selectedOption)}/>}/>
                 </div>
+
                 <div style={{marginBottom: '20px'}}>
                     <label className="formLabel"> Papéis </label>
                     <Field label="papeis"
                            placeholder="Papéis"
                            name="papeis"
-                           component={SelectMany}/>
+                           component={props =>
+                               <SelectMany
+                                   value={props.input.value}
+                                   options={[
+                                       {value: 'chocolate', label: 'Chocolate'},
+                                       {value: 'strawberry', label: 'Strawberry'},
+                                       {value: 'vanilla', label: 'Vanilla'}
+                                   ]}
+                                   placeholder="Duração"
+                                   handleChange = {(selectedOption) => props.input.onChange(selectedOption)}/>}/>
                 </div>
 
                 <div align="center" style={{marginBottom: '20px'}}>
-                    <Field label="papeis"
-                           placeholder="Papéis"
-                           name="papeis"
+                    <Field label="calendar"
+                           placeholder="calendar"
+                           name="calendar"
                            component={Calendar}
                            style={{width: '40%'}}/>
                 </div>
@@ -81,7 +121,7 @@ class AddTarefa extends Component {
 
 AddTarefa.propTypes = {
     handleSubmit: PropTypes.func,
-    onSubmit : PropTypes.func
+    onSubmit: PropTypes.func
 };
 export default reduxForm({
     form: 'addTarefa',
