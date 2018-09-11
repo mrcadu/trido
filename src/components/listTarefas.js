6e9f8145aef0f626cc9095cb735cd2707f235c69
cadu <carlosvicc14@gmail.com>
2018-09-11 14:58:53 -0300

    Criando a possibilidade de visualizar dia da semana da tarefa no listTarefas

--- src/components/listTarefas.js
+++ src/components/listTarefas.js
@@ -3,11 +3,13 @@ import Button from './button';
 import WihTarefas from "../HOC/wihTarefas";
 
 const ListTarefas = ({tarefas, handleClick}) => {
+  let diasDaSemana = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
   return (<div>
     {tarefas.map((tarefa) => {
-      return <div>
+      return <div>c
         <h4> {tarefa.nome} </h4>
         <h4> {tarefa.duracao} </h4>
+        <h4> {diasDaSemana[new Date(tarefa.data).getDay() + 1]} </h4>
         <Button style={{}} label="Terminar Tarefa" onClick={(e) => handleClick(e,tarefa)} />
       </div>
     })}
